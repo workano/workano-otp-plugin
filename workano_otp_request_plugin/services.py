d83972ac753edbee0f99eccc055a47283364e23e
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-07-07 20:47:38 +0330

    set tenant

--- workano_otp_request_plugin/services.py
+++ workano_otp_request_plugin/services.py
@@ -43,6 +43,7 @@ class OtpPlaybackService:
             expiration=365 * 24 * 60 * 60)['token']
         self.calld_client.set_token(token)
         self.confd_client.set_token(token)
+        self.confd.client.set_tenant('3fea6514-4154-4304-9b82-d90a8ca92405')
 
     def process_otp_request(self, params):
         print(
