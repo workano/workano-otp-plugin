0b9e4fbd3dcaf451e5a8e3d3d2b74472522f4773
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-07-07 22:48:15 +0330

    old db.py

--- workano_otp_request_plugin/db.py
+++ workano_otp_request_plugin/db.py
@@ -4,19 +4,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy_utils import database_exists, create_database
-from xivo_dao.helpers.db_manager import Base, Session   # reuse them
 
-# Base = declarative_base()
+Base = declarative_base()
 
 logger = logging.getLogger(__name__)
-# ScopedSession = scoped_session(sessionmaker())
+ScopedSession = scoped_session(sessionmaker())
 
 
 def init_db(db_uri):
-    engine = create_engine(db_uri, pool_pre_ping=True)
+    engine = create_engine(db_uri)
     if not database_exists(engine.url):
         logger.info('creating db')
         create_database(engine.url)
-    Session.configure(bind=engine)
-    Base.metadata.bind = engine
-    Base.metadata.create_all(engine, checkfirst=True)
+    Base.metadata.create_all(engine)
+    ScopedSession.configure(bind=engine)
