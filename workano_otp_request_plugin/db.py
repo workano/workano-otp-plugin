eda1d4d5f1ac9f33a1cf9a41ebc8c1b7e4f32621
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-07-07 22:45:00 +0330

    check first

--- workano_otp_request_plugin/db.py
+++ workano_otp_request_plugin/db.py
@@ -14,9 +14,9 @@ logger = logging.getLogger(__name__)
 
 def init_db(db_uri):
     engine = create_engine(db_uri, pool_pre_ping=True)
-    # if not database_exists(engine.url):
-    #     logger.info('creating db')
-    #     create_database(engine.url)
+    if not database_exists(engine.url):
+        logger.info('creating db')
+        create_database(engine.url)
     Session.configure(bind=engine)
     Base.metadata.bind = engine
-    Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine, checkfirst=True)
