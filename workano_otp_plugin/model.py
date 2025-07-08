05f485109e5030425121fad90f50b3208e754ce3
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-07-08 13:25:00 +0330

    update date to datetime

--- workano_otp_plugin/model.py
+++ workano_otp_plugin/model.py
@@ -2,7 +2,7 @@ from sqlalchemy import (
     Column
 )
 from sqlalchemy import text
-from sqlalchemy.types import (String, Boolean, Date)
+from sqlalchemy.types import (String, Boolean, DateTime,)
 from xivo_dao.helpers.db_manager import UUIDAsString
 from sqlalchemy.types import JSON
 
@@ -22,7 +22,7 @@ class OtpModel(Base):
     language = Column(String(128), nullable=False)
     uris = Column(JSON, nullable=True)
     status = Column(String(128), nullable=False)
-    creation_time = Column(Date, nullable=False)
-    end_time = Column(Date, nullable=True)
-    answer_time = Column(Date, nullable=True)
+    creation_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=True)
+    answer_time = Column(DateTime, nullable=True)
     talking_to = Column(JSON, nullable=True)
