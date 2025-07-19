d84b610204358c1715626f854812628ef6cfadcb
Ali Kazemkhanloo <alikazemkhanloo@gmail.com>
2025-07-19 18:29:02 +0330

    .

--- workano_otp_plugin/plugin.py
+++ workano_otp_plugin/plugin.py
@@ -58,7 +58,7 @@ class Plugin:
         )
         api.add_resource(
             OtpReportItemResource,
-            '/otp/report/<str:uuid>',
+            '/otp/report/<uuid:uuid>',
             resource_class_args=(otp_request_service,)
         )
     def unload(self):
